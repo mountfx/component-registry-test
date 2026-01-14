3543418b43cad12fcb60ad517cb1c0350f2ee0cf
Tobias Fuchsberger <mountfx@Tobiass-MacBook-Pro.local>
2026-01-14 17:26:23 +0100

    adds globals.css

--- registry/konverto/button/Button.vue
+++ registry/konverto/button/Button.vue
@@ -1,21 +1,5 @@
 <template>
-  <button><slot name="text" /></button>
+  <button class="px-4 py-3 rounded-md bg-main hover:bg-accent text-ambient">
+    <slot name="text" />
+  </button>
 </template>
-
-<style scoped>
-button {
-  background-color: tomato;
-  border-radius: 24px;
-  padding: 24px 32px;
-  outline: none;
-  border: unset;
-  font-size: 16px;
-  color: white;
-}
-button:focus {
-  background-color: blue;
-}
-button:active {
-  background-color: crimson;
-}
-</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from "node:path";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  base: "https://mountfx.github.io/component-registry-test/",
-  plugins: [vue()],
+  base: "/component-registry-test/",
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
